b8dbf20564389487451e5059ad8806ed5aa9433f
Paweł Rup <pawelrup@lobocode.pl>
2020-02-20 14:42:57 +0100

    Add required dependencies

--- Package.swift
+++ Package.swift
@@ -1,28 +1,27 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let package = Package(
-    name: "PassKit",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "PassKit",
-            targets: ["PassKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "PassKit",
-            dependencies: []),
-        .testTarget(
-            name: "PassKitTests",
-            dependencies: ["PassKit"]),
-    ]
-)
+// Dependencies declare other packages that this package depends on.
+let dependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
+    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-beta"),
+    .package(url: "https://github.com/vapor/apns.git", from: "1.0.0-beta"),
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+    .package(url: "https://github.com/pawelrup/PassGenerator", .branch("remove-vapor-dependency"))
+]
+
+// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+// Targets can depend on other targets in this package, and on products in packages which this package depends on.
+let targets: [Target] = [
+    .target(name: "PassKit", dependencies: ["Fluent", "Vapor", "APNS", "Logging", "PassGenerator"]),
+    .testTarget(name: "PassKitTests", dependencies: ["PassKit"])
+]
+
+// Products define the executables and libraries produced by a package, and make them visible to other packages.
+let products: [Product] = [
+    .library(name: "PassKit", targets: ["PassKit"])
+]
+
+let package = Package(name: "PassKit", platforms: [.macOS(.v10_15)], products: products, dependencies: dependencies, targets: targets)
