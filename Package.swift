236c2458ea415f2b433dabc2cb1a8263e6098f22
Paweł Rup <pawelrup@lobocode.pl>
2020-06-16 22:44:28 +0200

    Packages update

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 
 // Dependencies declare other packages that this package depends on.
 let dependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/vapor/vapor.git", from: "4.8.0"),
-    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc.3.1"),
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.10.0"),
+    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
     .package(url: "https://github.com/vapor/apns.git", from: "1.0.0-rc.1.1"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
-    .package(url: "https://github.com/pawelrup/PassGenerator.git", .exact("0.11.3"))
+    .package(url: "https://github.com/pawelrup/PassGenerator.git", .exact("0.11.4"))
 ]
 
 // Targets are the basic building blocks of a package. A target can define a module or a test suite.
